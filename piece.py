e2ef2315338e80498df2f7638705fabe79275702
Tobi <51411393+7ubi@users.noreply.github.com>
2020-06-17 19:37:06 +0200

    vertical / horizontal collision detection for rook and queen

--- piece.py
+++ piece.py
@@ -5,52 +5,91 @@ pygame.init()
 
 class Piece:
 
-	def __init__(self, color, x, y, w, h):
-		self.color = color
-		self.x = x
-		self.y = y
-		self.selected = False
-		self.w = int(w)
-		self.h = int(h)
-		self.blackImg
-		self.whiteImg
+    def __init__(self, color, x, y, w, h):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.selected = False
+        self.w = int(w)
+        self.h = int(h)
+        self.blackImg
+        self.whiteImg
 
-	def setSelected(self, set):
-		self.selected = set
+    def setSelected(self, set):
+        self.selected = set
 
-	def clickedOn(self, pos):
-		x, y = pos
-		if x > self.x and x < self.x + self.w and y > self.y and y < self.y + self.h:
-			return True
-		return False
+    def clickedOn(self, pos):
+        x, y = pos
+        if x > self.x and x < self.x + self.w and y > self.y and y < self.y + self.h:
+            return True
+        return False
 
-	def move(self, pos, field, width, height):
-		nx, ny = pos
-		if self.selected:
-			x = int(nx / self.w) * self.w
-			y = int(ny / self.h) * self.h
-    		
-			canBePlaced = self.canBePlaced(x, y, field)
+    def move(self, pos, field, width, height):
+        nx, ny = pos
+        if self.selected:
+            x = int(nx / self.w) * self.w
+            y = int(ny / self.h) * self.h
 
-			if field[int(x / width * 8)][int(y / height * 8)] == self.color:
-				return False
+            canBePlaced = self.canBePlaced(x, y, field)
 
-			if canBePlaced:
-				self.x = x
-				self.y = y
-				return True
+            if field[int(x / width * 8)][int(y / height * 8)] == self.color:
+                return False
 
+            if canBePlaced:
+                self.x = x
+                self.y = y
+                return True
 
+    def horverMove(self, x, y, field):
+        if self.y == y:
+            d = self.x - x
+            i = int(self.x / (self.w * 8) * 8)
+            j = int(x / (self.w * 8) * 8)
+            m = int(self.y / (self.h * 8) * 8)
+            print(d)
+            if d < 0:
+                i += 1
+                for k in range(i, j, 1):
+                    if field[int(k)][m] != 0:
+                        return False
+            else:
+                i -= 1
+                for k in range(i, j, -1):
+                    if field[int(k)][m] != 0:
+                        return False
 
-	def show(self, screen):
-		if self.selected:
-			pygame.draw.rect(screen, pygame.Color(255, 0, 0), (self.x, self.y, self.w, self.h))
+            return True
 
+        if self.x == x:
+            d = self.y - y
+            i = int(self.y / (self.h * 8) * 8)
+            j = int(y / (self.h * 8) * 8)
+            m = int(self.x / (self.w * 8) * 8)
+            if d < 0:
+                i += 1
+                for k in range(i, j, 1):
+                	print(field[m][k])
+                	if field[m][k] != 0:
+                		return False
+            else:
+                i -= 1
+                for k in range(i, j, -1):
+                	print(field[m][k])
+                	if field[m][k] != 0:
+                		return False
 
-		if self.color == 1:
-			screen.blit(pygame.transform.scale(self.blackImg, (self.w, self.h)), (self.x, self.y))
+            return True
+        return False
 
-		if self.color == -1:
-			screen.blit(pygame.transform.scale(self.whiteImg, (self.w, self.h)), (self.x, self.y))
+    def show(self, screen):
+        if self.selected:
+            pygame.draw.rect(screen, pygame.Color(255, 0, 0),
+                             (self.x, self.y, self.w, self.h))
 
+        if self.color == 1:
+            screen.blit(pygame.transform.scale(
+                self.blackImg, (self.w, self.h)), (self.x, self.y))
 
+        if self.color == -1:
+            screen.blit(pygame.transform.scale(
+                self.whiteImg, (self.w, self.h)), (self.x, self.y))
--- pieces.py
+++ pieces.py
@@ -29,8 +29,7 @@ class Rook(Piece):
         super().__init__(color, x, y, w, h)
 
     def canBePlaced(self, x, y, field):
-    	if self.x == x or self.y == y:
-    		return True
+    	return self.horverMove(x, y, field)
     	return False
 
 class Bishop(Piece):
@@ -75,43 +74,8 @@ class Queen(Piece):
 		if int(distX) == int(distY):
 			return True
 
-		if self.y == y:
-			d = self.x - x
-			print(self.x, x)
-			i = int(self.x / (self.w * 8) * 8)
-			j = int(x / (self.w * 8) * 8)
-			m = int(self.y / (self.h * 8) * 8)
-			print(i, j)
-			if d < 0:
-				i += 1
-				for k in range(i, j, 1):
-					if field[int(k)][m] != 0:
-						return False 		
-			else:
-				i -= 1
-				for k in range(i, j, -1):
-					if field[int(k)][m] != 0:
-						return False 			
-			return True 
-		if self.x == x:
-			d = self.y - y
-			i = int(self.y / (self.h * 8) * 8)
-			j = int(y / (self.h * 8) * 8)
-			m = int(self.x / (self.w * 8) * 8)
+		return self.horverMove(x, y, field)
 
-			if d < 0:
-				i += 1
-				
-				for k in range(i, j, 1):
-					if field[m][k] != 0:
-						return False 		
-			else:
-				i -= 1
-				print(i, j)
-				for k in range(i, j, -1):
-					if int(field[m][int(k)]) != 0:
-						return False 			
-			return True 
 		return False
 
 class Knight(Piece):
